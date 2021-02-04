efcff742ea75c77e69cd58b629865546587a9e67
Your Name <nguenviethoang2000@gmail.com>
2021-02-04 11:17:35 +0700

    fix new

--- lib/view/new_splash.dart
+++ lib/view/new_splash.dart
@@ -1,84 +1,6 @@
-import 'package:azt/view/mainHome.dart';
+import 'package:azt/view/splash_screen.dart';
 import 'package:flutter/material.dart';
-import 'package:loading_indicator/loading_indicator.dart';
-// import 'package:loading_indicator/loading_indicator.dart';
-// void main() {
-//   runApp(NewSplash());
-// }
 
-// class NewSplash extends StatefulWidget {
-//   @override
-//   _NewSplashState createState() => _NewSplashState();
-// }
-
-// class _NewSplashState extends State<NewSplash> {
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       backgroundColor: Color(0xFFecf0f5),
-//       body: Center(
-//         child: Column(
-//           mainAxisAlignment: MainAxisAlignment.spaceBetween,
-//           crossAxisAlignment: CrossAxisAlignment.center,
-//           children: <Widget>[
-//             Padding(
-//               padding: EdgeInsets.only(top: 0),
-//             ),
-// Column(
-//   children: [
-// Image(
-//   image: AssetImage('assets/icon_app_512.png'),
-//   height: 100,
-//   color: Color(0xFF42C0B6),
-// ),
-//     Image(
-//       image: AssetImage('assets/azota-logo-text.png'),
-//       height: 70,
-//     ),
-//     Padding(
-//       padding: EdgeInsets.only(right: 80, left: 80, top: 20),
-//       child: Text(
-//         'Một cách thật đơn giản để',
-//         style: TextStyle(fontSize: 16),
-//       ),
-//     ),
-//     Padding(
-//       padding: EdgeInsets.only(right: 0),
-//       child: Text(
-//         'giao bài tập',
-//         style: TextStyle(fontSize: 16),
-//       ),
-//     ),
-//   ],
-// ),
-//             SizedBox(
-//               height: 70,
-//               child: LoadingIndicator(
-//                 indicatorType: Indicator.ballClipRotatePulse,
-//                 color: Color(0xFF42C0B6),
-//               ),
-//             ),
-//             Container(
-//               child: Column(
-//                 children: [
-//                   Text('Phiên bản: 1.0'),
-//                   Padding(
-//                     padding: EdgeInsets.only(left: 40, right: 40, top: 10),
-//                     child: Text(
-//                       'Được phát triển bởi GET.jsc',
-//                       textAlign: TextAlign.center,
-//                     ),
-//                   )
-//                 ],
-//               ),
-//               padding: const EdgeInsets.all(20.0),
-//             ),
-//           ],
-//         ),
-//       ),
-//     );
-//   }
-// }
 import 'package:splashscreen/splashscreen.dart';
 
 void main() {
@@ -97,7 +19,7 @@ class _NewSplashState extends State<NewSplash> {
   Widget build(BuildContext context) {
     return new SplashScreen(
       seconds: 3,
-      // navigateAfterSeconds: new MainHome(),
+      navigateAfterSeconds: new Splash(),
       title: new Text(
         'Một cách thật đơn giản để \ngiao bài tập.',
         style: TextStyle(fontSize: 16, color: Colors.black45),
@@ -112,15 +34,6 @@ class _NewSplashState extends State<NewSplash> {
       onClick: () => print("Flutter Egypt"),
       loaderColor: Colors.redAccent,
       loadingText: Text(
-        '\n'
-        '\n'
-        '\n'
-        '\n'
-        '\n'
-        '\n'
-        '\n'
-        '\n'
-        '\n'
         '\n'
         'Phiên bản: 1.0'
         '\n'
