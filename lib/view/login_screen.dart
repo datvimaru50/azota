c0bd1c786fe8d02e5146bfe1b0f5fc778f96b960
nguyen hoang <hoangnvh@outlook.com.vn>
2021-02-26 11:21:58 +0700

    dialog 2

--- lib/view/login_screen.dart
+++ lib/view/login_screen.dart
@@ -113,7 +113,8 @@ class _LoginFormState extends State<LoginForm> {
       body: ListView(
         children: [
           Container(
-            height: MediaQuery.of(context).copyWith().size.height / 1,
+            color: Colors.blue,
+            height: MediaQuery.of(context).copyWith().size.height / 1.11,
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               crossAxisAlignment: CrossAxisAlignment.center,
