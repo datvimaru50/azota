51627cf18f3cbfcda503ae760ceb007eb0bfb6ed
nguyen hoang <hoangnvh@outlook.com.vn>
2021-03-01 14:16:49 +0700

    thêm nút đăng nhập apple

--- lib/view/login_screen.dart
+++ lib/view/login_screen.dart
@@ -2,7 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 import 'package:azt/view/notificationScreen.dart';
 // import 'package:azt/view/register_screen.dart';
+import 'dart:io';
 
+import 'package:http/http.dart' as http;
+
+import 'package:sign_in_with_apple/sign_in_with_apple.dart';
 import 'package:azt/controller/login_controller.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
@@ -264,6 +268,59 @@ class _LoginFormState extends State<LoginForm> {
                           '---Hoặc---',
                           style: TextStyle(fontSize: 15),
                         ),
+                      ),
+                      SignInWithAppleButton(
+                        onPressed: () async {
+                          final credential =
+                              await SignInWithApple.getAppleIDCredential(
+                            scopes: [
+                              AppleIDAuthorizationScopes.email,
+                              AppleIDAuthorizationScopes.fullName,
+                            ],
+                            webAuthenticationOptions: WebAuthenticationOptions(
+                              // ignore: todo
+                              // TODO: Set the `clientId` and `redirectUri` arguments to the values you entered in the Apple Developer portal during the setup
+                              clientId:
+                                  'com.aboutyou.dart_packages.sign_in_with_apple.example',
+                              redirectUri: Uri.parse(
+                                'https://flutter-sign-in-with-apple-example.glitch.me/callbacks/sign_in_with_apple',
+                              ),
+                            ),
+                            // ignore: todo
+                            // TODO: Remove these if you have no need for them
+                            nonce: 'example-nonce',
+                            state: 'example-state',
+                          );
+
+                          print(credential);
+
+                          // This is the endpoint that will convert an authorization code obtained
+                          // via Sign in with Apple into a session in your system
+                          final signInWithAppleEndpoint = Uri(
+                            scheme: 'https',
+                            host:
+                                'flutter-sign-in-with-apple-example.glitch.me',
+                            path: '/sign_in_with_apple',
+                            queryParameters: <String, String>{
+                              'code': credential.authorizationCode,
+                              'firstName': credential.givenName,
+                              'lastName': credential.familyName,
+                              'useBundleId': Platform.isIOS || Platform.isMacOS
+                                  ? 'true'
+                                  : 'false',
+                              if (credential.state != null)
+                                'state': credential.state,
+                            },
+                          );
+
+                          final session = await http.Client().post(
+                            signInWithAppleEndpoint,
+                          );
+
+                          // If we got this far, a session based on the Apple ID credential has been created in your system,
+                          // and you can now set this as the app's session
+                          print(session);
+                        },
                       ),
                       Padding(
                         padding: const EdgeInsets.symmetric(vertical: 5.0),
