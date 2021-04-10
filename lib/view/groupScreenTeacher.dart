b71bd7feb1fc31a31e0d8d3974f5b5fbf3792fa0
dat vu quoc <datvimaru50@gmail.com>
2021-04-10 14:29:50 +0700

    fix missing loading icon on dashboard

--- lib/view/groupScreenTeacher.dart
+++ lib/view/groupScreenTeacher.dart
@@ -4,7 +4,6 @@ import 'package:azt/view/notificationScreen.dart';
 import 'package:azt/view/userProfile.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:azt/controller/notification_controller.dart';
 
 class GroupScreenTeacher extends StatefulWidget {
   @override
@@ -40,84 +39,81 @@ class _GroupScreenTeacherState extends State<GroupScreenTeacher> {
       // use noti data from store provider
     return Scaffold(
       body: _widgetOptions.elementAt(_selectedIndex),
-      bottomNavigationBar: FutureBuilder(
-        future: notiData,
-        builder: (context, snapshot){
-          if(snapshot.hasData){
-            return BottomNavigationBar(
-              items: <BottomNavigationBarItem>[
-                BottomNavigationBarItem(
-                  icon: Icon(Icons.room_preferences_outlined),
-                  label: 'Lớp học',
-                  backgroundColor: Color(0xff17A2B8),
+      bottomNavigationBar: BottomNavigationBar(
+        items: <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.room_preferences_outlined),
+            label: 'Lớp học',
+            backgroundColor: Color(0xff17A2B8),
+          ),
+
+          BottomNavigationBarItem(
+            // icon: Icon(Icons.notifications_none),
+            icon: Stack(
+              children: [
+                Icon(
+                  Icons.notifications_none,
+                  size: 30,
+                ),
+                FutureBuilder(
+                    future: notiData,
+                    builder: (context, snapshot){
+                      if(snapshot.hasData){
+                        return Consumer<NotiModel>(
+                          builder: (context, noti, child){
+                            return noti.totalUnread == 0? Container(width: 0, height: 0,) : Container(
+                              width: 30,
+                              height: 30,
+                              alignment: Alignment.topRight,
+                              child: Container(
+                                width: 16,
+                                height: 16,
+                                decoration: BoxDecoration(
+                                  shape: BoxShape.circle,
+                                  color: Color(0xffc32c37),
+                                ),
+                                child: Padding(
+                                  padding: const EdgeInsets.all(0.0),
+                                  child: Center(
+                                    child: Text(
+                                      '${noti.totalUnread}', // <========== access store data
+                                      style: TextStyle(fontSize: 10, color: Colors.white),
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            );
+                          },
+                        );
+                      } else if(snapshot.hasError){
+                        return Container(width: 0, height: 0,);
+                      }
+                      return Container(width: 0, height: 0,);
+                    }
                 ),
 
-                BottomNavigationBarItem(
-                  // icon: Icon(Icons.notifications_none),
-                  icon: Stack(
-                    children: [
-                      Icon(
-                        Icons.notifications_none,
-                        size: 30,
-                      ),
-                      Container(
-                        width: 30,
-                        height: 30,
-                        alignment: Alignment.topRight,
-                        child: Container(
-                          width: 16,
-                          height: 16,
-                          decoration: BoxDecoration(
-                            shape: BoxShape.circle,
-                            color: Color(0xffc32c37),
-                          ),
-                          child: Padding(
-                            padding: const EdgeInsets.all(0.0),
-                            child: Center(
-                              child: Consumer<NotiModel>(
-                                builder: (context, noti, child){
-                                  return Text(
-                                    '${noti.totalUnread}', // <========== access store data
-                                    style: TextStyle(fontSize: 10, color: Colors.white),
-                                  );
-                                },
-                              ),
-                            ),
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                  label: 'Thông báo',
-                  backgroundColor: Color(0xff17A2B8),
-                ),
-                BottomNavigationBarItem(
-                  icon: Icon(Icons.menu_book),
-                  label: 'Nội dung',
-                  backgroundColor: Color(0xff17A2B8),
-                ),
-                BottomNavigationBarItem(
-                  icon: Icon(Icons.account_circle_outlined),
-                  label: 'Tài khoản',
-                  backgroundColor: Color(0xff17A2B8),
-                ),
               ],
-              backgroundColor: Color(0xff17A2B8),
-              selectedItemColor: Colors.white,
-              unselectedItemColor: Color(0xff6cdbed),
-              currentIndex: _selectedIndex,
-              onTap: _onItemTapped,
-            );
-          } else if(snapshot.hasError){
-            return Center(
-              child: Text("${snapshot.error}"),
-            );
-          }
-          return Center(
-            child: Text(''),
-          );
-        },
-      )
+            ),
+            label: 'Thông báo',
+            backgroundColor: Color(0xff17A2B8),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.menu_book),
+            label: 'Nội dung',
+            backgroundColor: Color(0xff17A2B8),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.account_circle_outlined),
+            label: 'Tài khoản',
+            backgroundColor: Color(0xff17A2B8),
+          ),
+        ],
+        backgroundColor: Color(0xff17A2B8),
+        selectedItemColor: Colors.white,
+        unselectedItemColor: Color(0xff6cdbed),
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
+      ),
     );
   }
 }
